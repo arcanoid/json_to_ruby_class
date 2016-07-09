3a68add2c6130be99482fccbfff9e079e9c9408b
Vasilis Kalligas <billkall@gmail.com>
2016-07-09 17:27:53 +0300

    Added proper code to produce proper class models.

--- json_to_ruby_class.gemspec
+++ json_to_ruby_class.gemspec
@@ -9,9 +9,8 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Vasilis Kalligas"]
   spec.email         = ["billkall@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{This gem can be used in Rails apps to automatically convert a json to the relative model classes}
+  spec.homepage      = "https://rubygems.org/gems/json_to_ruby_class"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
--- lib/json_to_ruby_class.rb
+++ lib/json_to_ruby_class.rb
@@ -1,5 +1,58 @@
 require "json_to_ruby_class/version"
 
 module JsonToRubyClass
-  # Your code goes here...
+  def self.produce_models(hash)
+    models_array = collect_info_from_json(hash, nil)
+    prepare_models_from_hash models_array
+  end
+
+  #######
+  private
+  #######
+
+  def self.collect_info_from_json(hash, model_name, existing_models_array = [])
+    accessors = []
+
+    hash.each do |key, value|
+      # In some cases you have a parent called Resources and inside has an array of models Resource
+      constructed_model_name = (key.to_s == model_name.to_s ? "#{model_name}_#{key}" : key)
+
+      if value.class == Hash
+        collect_info_from_json(value, constructed_model_name, existing_models_array)
+      elsif value.class == Array
+        value.each do |array_element|
+          collect_info_from_json(array_element, constructed_model_name, existing_models_array)
+        end
+      end
+
+      accessors << key
+    end
+
+    model_name_to_be_used = (model_name.nil? ? 'Example' : model_name.to_s.camelcase)
+    accessors_to_be_used = accessors.map { |s| ":#{s.to_s.underscore}" }
+
+    if (hash = existing_models_array.find { |model| model[:name] == model_name_to_be_used })
+      hash[:accessors].push(accessors_to_be_used).uniq!
+    else
+      existing_models_array << {
+          :name => (model_name.nil? ? 'Example' : model_name.to_s.camelcase),
+          :accessors => accessors_to_be_used
+      }
+    end
+
+    existing_models_array
+  end
+
+  def self.prepare_models_from_hash(models_array)
+    model_string = ''
+
+    models_array.each do |model|
+      model_string << "Class #{model[:name].singularize}\n"
+      model_string << '   attr_accessor '
+      model_string << model[:accessors].join(",\n                 ")
+      model_string << "\nend\n\n"
+    end
+
+    model_string
+  end
 end
